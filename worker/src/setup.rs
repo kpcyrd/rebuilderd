34f29c20c1c3a9c81827544396265c939b7ca448
kpcyrd <git@rxv.cc>
2020-04-15 21:33:25 +0200

    Correctly use private repro build root

--- worker/src/setup.rs
+++ worker/src/setup.rs
@@ -3,18 +3,18 @@ use std::env;
 use std::fs;
 
 pub fn run(name: &str) -> Result<()> {
-    let config_path = format!("/etc/rebuilderd-worker/{}", name);
-    let work_directory = format!("/var/lib/rebuilderd-worker/{}", name);
+    let config_dir = format!("/etc/rebuilderd-worker/{}", name);
+    let work_dir = format!("/var/lib/rebuilderd-worker/{}", name);
+    let repro_work_dir = format!("{}/repro", work_dir);
 
-    info!("writing worker default config to {}", config_path);
-    fs::create_dir_all(&format!("{}/archlinux-repro", config_path))?;
-    fs::write(&format!("{}/archlinux-repro/repro.conf", config_path), format!("BUILDDIRECTORY={}\n", work_directory))?;
+    info!("writing worker default config to {}", config_dir);
+    fs::create_dir_all(&format!("{}/archlinux-repro", config_dir))?;
+    fs::write(&format!("{}/archlinux-repro/repro.conf", config_dir), format!("BUILDDIRECTORY={}\n", repro_work_dir))?;
 
-    let work_directory = format!("/var/lib/rebuilderd-worker/{}", name);
-    info!("switching into work directory: {}", work_directory);
-    fs::create_dir_all(&work_directory)?;
-    env::set_current_dir(&work_directory)?;
-    env::set_var("XDG_CONFIG_HOME", work_directory);
+    info!("switching into work directory: {}", work_dir);
+    fs::create_dir_all(&repro_work_dir)?;
+    env::set_current_dir(&work_dir)?;
+    env::set_var("XDG_CONFIG_HOME", config_dir);
 
     Ok(())
 }
