fd7e2c9a7dec4271017a858a3c38c58da1e213f9
kpcyrd <git@rxv.cc>
2020-04-26 00:29:32 +0200

    Set ping interval back to 60s

--- common/src/config.rs
+++ common/src/config.rs
@@ -7,7 +7,7 @@ use std::path::{Path, PathBuf};
 
 pub const IDLE_DELAY: u64 = 180;
 pub const PING_DEADLINE: i64 = IDLE_DELAY as i64 + 20;
-pub const PING_INTERVAL: u64 = 30;
+pub const PING_INTERVAL: u64 = 60;
 pub const WORKER_DELAY: u64 = 3;
 pub const API_ERROR_DELAY: u64 = 30;
 
