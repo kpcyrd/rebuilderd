f9ea5fe1f44dcae3cb2191347d2ee894a1ba7763
Jelle van der Waa <jelle@vdwaa.nl>
2021-03-16 18:47:56 +0100

    If-Modified-Since header support for /api/v0/pkgs/list
    When the If-Modified-Since header is set the given date is compared to
    the last built package date. Invalid dates are silenty ignored, and if a
    date in the future is given in comparison to the last built package date
    HTTP 304 Not Modified is returned.

--- daemon/src/api.rs
+++ daemon/src/api.rs
@@ -13,6 +13,7 @@ use rebuilderd_common::api::*;
 use rebuilderd_common::errors::*;
 use std::net::IpAddr;
 use std::sync::{Arc, RwLock};
+use std::time::SystemTime;
 
 fn forbidden() -> web::Result<HttpResponse> {
     Ok(HttpResponse::Forbidden()
@@ -24,12 +25,22 @@ fn not_found() -> web::Result<HttpResponse> {
         .body("Not found\n"))
 }
 
+fn not_modified() -> web::Result<HttpResponse> {
+    Ok(HttpResponse::NotModified().body(""))
+}
+
 pub fn header<'a>(req: &'a HttpRequest, key: &str) -> Result<&'a str> {
     let value = req.headers().get(key)
         .ok_or_else(|| format_err!("Missing header"))?
         .to_str()
         .context("Failed to decode header value")?;
     Ok(value)
+}
+
+fn modified_since_duration(req: &HttpRequest, datetime: DateTime<Utc>) -> Option<chrono::Duration> {
+    header(req, &http::header::IF_MODIFIED_SINCE.as_str()).ok()
+        .and_then(|value| chrono::DateTime::parse_from_rfc2822(value).ok())
+        .map(|value| value.signed_duration_since(datetime))
 }
 
 #[get("/api/v0/workers")]
@@ -79,6 +90,7 @@ fn opt_filter(this: &str, filter: Option<&str>) -> bool {
 
 #[get("/api/v0/pkgs/list")]
 pub async fn list_pkgs(
+    req: HttpRequest,
     query: web::Query<ListPkgs>,
     pool: web::Data<Pool>,
 ) -> web::Result<impl Responder> {
@@ -86,14 +98,18 @@ pub async fn list_pkgs(
     let mut builder = HttpResponse::Ok();
 
     // Set Last-Modified header to the most recent build package time
-    match models::Package::most_recent_built_at(connection.as_ref())? {
-        None => {},
-        Some(built_at) => {
-            let datetime: DateTime<Utc> = DateTime::from_utc(built_at, Utc);
-            builder.set(http::header::LastModified(std::time::SystemTime::from(datetime).into()));
+    // If If-Modified-Since header is set, compare it to the latest built time.
+    if let Some(latest_built_at) = models::Package::most_recent_built_at(connection.as_ref())? {
+        let latest_built_at = DateTime::from_utc(latest_built_at, Utc);
+        if let Some(duration) = modified_since_duration(&req, latest_built_at) {
+            if duration.num_seconds() >= 0 {
+                return not_modified();
+            }
         }
+        let latest_built_at = SystemTime::from(latest_built_at);
+        builder.set(http::header::LastModified(latest_built_at.into()));
     }
-
+ 
     let mut pkgs = Vec::<PkgRelease>::new();
     for pkg in models::Package::list(connection.as_ref())? {
         if opt_filter(&pkg.name, query.name.as_deref()) {
