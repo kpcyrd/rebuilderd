8b58ac3127f7d60c72aa1528282a86ed5b8328bf
Holger Levsen <holger@layer-acht.org>
2024-11-20 12:14:10 +0100

    update rebuilder-debian.sh for current debrebuild from devscripts
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- worker/rebuilder-debian.sh
+++ worker/rebuilder-debian.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -xe
-# debrebuild.py needs to be run from the repo
-/debrebuild/debrebuild.py --output="${REBUILDERD_OUTDIR}" --builder=mmdebstrap --use-metasnap -- "${1}"
+cd "$(dirname "$1")"
+# for production it's useful to call debrebuild with --cache="$directory"
+debrebuild --buildresult="${REBUILDERD_OUTDIR}" --builder=sbuild+unshare -- "${1}"
