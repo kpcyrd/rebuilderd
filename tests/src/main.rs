538f2acefa28c8be6ccfab413882239252cc9f2b
kpcyrd <git@rxv.cc>
2025-08-18 22:28:24 +0200

    Add integration test to ensure attestations are stored, signed and published

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -3,12 +3,14 @@ use clap::Parser;
 use colored::Colorize;
 use env_logger::Env;
 use in_toto::crypto::{KeyType, PrivateKey, SignatureScheme};
+use rebuilderd::attestation::Attestation;
 use rebuilderd::config::Config;
 use rebuilderd_common::api::*;
 use rebuilderd_common::config::*;
 use rebuilderd_common::errors::*;
 use rebuilderd_common::Status;
 use rebuilderd_common::{PkgArtifact, PkgGroup, PkgRelease};
+use serde_json::json;
 use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
@@ -79,10 +81,7 @@ async fn test<T: Sized>(label: &str, f: impl futures::Future<Output = Result<T>>
 }
 
 #[actix_web::main]
-async fn spawn_server(config: Config) {
-    let privkey = PrivateKey::new(KeyType::Ed25519).expect("Failed to generate private key");
-    let privkey = PrivateKey::from_pkcs8(&privkey, SignatureScheme::Ed25519)
-        .expect("Failed to use generated private key");
+async fn spawn_server(config: Config, privkey: PrivateKey) {
     if let Err(err) = rebuilderd::run_config(config, privkey).await {
         error!("daemon errored: {:#}", err);
     }
@@ -126,6 +125,11 @@ async fn main() -> Result<()> {
         },
     );
 
+    let privkey = PrivateKey::new(KeyType::Ed25519).expect("Failed to generate private key");
+    let privkey = PrivateKey::from_pkcs8(&privkey, SignatureScheme::Ed25519)
+        .expect("Failed to use generated private key");
+    let pubkey = privkey.public().to_owned();
+
     if !args.no_daemon {
         let config = rebuilderd::config::from_struct(config.clone(), args.cookie)?;
 
@@ -135,7 +139,7 @@ async fn main() -> Result<()> {
 
         info!("Spawning server");
         thread::spawn(|| {
-            spawn_server(config);
+            spawn_server(config, privkey);
         });
         wait_for_server(&addr)?;
     }
@@ -391,6 +395,91 @@ async fn main() -> Result<()> {
         if pkgs.len() != 3 {
             bail!("Not 3");
         }
+        Ok(())
+    })
+    .await?;
+
+    test("Fetching task and reporting GOOD with attestation", async {
+        let task = client
+            .pop_queue(&WorkQuery {
+                supported_backends: vec!["rebuilderd".to_string()],
+            })
+            .await?;
+
+        let queue = match task {
+            JobAssignment::Rebuild(item) => *item,
+            _ => bail!("Expected a job assignment"),
+        };
+
+        let mut rebuilds = Vec::new();
+        for artifact in queue.pkgbase.artifacts.clone() {
+            let attestation = serde_json::to_string(&json!({
+                "signatures": [
+                    {
+                        "keyid": "c25d24c04760b6982de77736776edc6600d5f8e1e84d0bba2a7299959ce7d47f",
+                        "sig": "8cd70318ea1b34c91bf7303e9c8811df43d1b4746aa9adf1d503ebb0241e0fbff9be28f36dac0318825782bf05dbbcea7171eb0ca9a89be3b02666f0f3c84301"
+                    }
+                ],
+                "signed": {
+                    "_type": "link",
+                    "name": "rebuild spytrap-adb_0.3.5-1_amd64.deb",
+                    "materials": {
+                        "rust-spytrap-adb_0.3.5-1_amd64.buildinfo": {
+                            "sha512": "d130dbdbd51480f5cb79c1e6ce09fa61a69766e56725543b9c19bee8248306b2c3c2a2c66b250992bf20b2f5af7cf03bf401255104714bc9d654126fb41bc59f",
+                            "sha256": "9df2f9a721f5016874c5f78ae88d3df77f9e49ea6070f935bfeeb438cd73a158"
+                        }
+                    },
+                    "products": {
+                        "spytrap-adb_0.3.5-1_amd64.deb": {
+                            "sha256": "58a7d451d5d59fda6284a05418b99e34fab32d07e63d0b164404eaaed1317edd",
+                            "sha512": "f38806536701138cb1b2059565e5f73ec07288f9a3013ba986e33d510432e183e7bfe94af31bb8d480b85c84f4c145ed5c28c5949d618a4e94b2c7aecb309642"
+                        }
+                    },
+                    "environment": null,
+                    "byproducts": {},
+                    "command": []
+                },
+            }))?;
+            rebuilds.push((
+                artifact,
+                Rebuild {
+                    diffoscope: None,
+                    status: BuildStatus::Good,
+                    attestation: Some(attestation),
+                },
+            ));
+        }
+
+        let report = BuildReport {
+            queue,
+            build_log: String::new(),
+            rebuilds,
+        };
+        client.report_build(&report).await?;
+
+        Ok(())
+    })
+    .await?;
+
+    test("Fetching and verify attestation", async {
+        let pkgs = list_pkgs(&client).await?;
+
+        for pkg in pkgs {
+            if !pkg.has_attestation {
+                continue;
+            }
+
+            let Some(build_id) = pkg.build_id else {
+                continue;
+            };
+
+            let attestation = client.fetch_attestation(build_id).await?;
+            let attestation = Attestation::parse(&attestation)?;
+
+            // ensure the attestation verifies because the rebuilderd instance itself signed it too
+            attestation.verify(1, [&pubkey])?;
+        }
+
         Ok(())
     })
     .await?;
