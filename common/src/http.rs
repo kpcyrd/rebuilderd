a872ef950ae921fac17786057b6d98af6f2b2924
kpcyrd <git@rxv.cc>
2026-08-22 11:47:13 +0200

    Add http connect timeout and user-agent

--- common/src/http.rs
+++ common/src/http.rs
@@ -4,7 +4,9 @@ use std::time::Duration;
 
 pub fn client() -> Result<Client> {
     Client::builder()
+        .connect_timeout(Duration::from_secs(20))
         .read_timeout(Duration::from_secs(60))
+        .user_agent(concat!("rebuilderd/", env!("CARGO_PKG_VERSION")))
         .build()
         .map_err(Error::from)
 }
